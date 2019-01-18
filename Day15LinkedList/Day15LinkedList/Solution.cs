aa4f07d1b1c5c732a241dc49f17d57da440cbaac
Andes Lam <lamandes@outlook.com>
2019-01-18 18:58:34 +0000

    Completed Day 15 Linked List solution. Commit Code.

--- Day15LinkedList/Day15LinkedList/Solution.cs
+++ Day15LinkedList/Day15LinkedList/Solution.cs
@@ -8,8 +8,45 @@ namespace Day15LinkedList
 {
     class Solution
     {
-        static void Main(string[] args)
+        public static Node insert(Node head, int data)
         {
+            Node tail = new Node(data);         //1. Create a new node and insert data into it
+            if (head != null)                   //2. Check if head already assigned before, yes then change each pointer while it reach null
+            {
+                Node oringinalHead = head;      //3. Save original head node pointer
+                while (head.next != null)
+                {
+                    head = head.next;
+                }             
+                head.next = tail;               //4. Update last node and Insert the new node into last node
+                return oringinalHead;
+            }
+            else
+            {
+                return tail;
+            }
+        }
+
+        public static void display(Node head)
+        {
+            Node start = head;
+            while (start != null)
+            {
+                Console.Write(start.data + " ");
+                start = start.next;
+            }
+        }
+        static void Main(String[] args)
+        {
+
+            Node head = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                head = insert(head, data);
+            }
+            display(head);
         }
     }
 }
