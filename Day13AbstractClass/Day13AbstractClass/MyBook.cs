65365f3cd3980802b4c44cc58d102d1b7db3354c
Andes Lam <lamandes@outlook.com>
2019-01-18 16:00:09 +0000

    Create Day 13 Abstract Class. Save solution and commit code

---
+++ Day13AbstractClass/Day13AbstractClass/MyBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day13AbstractClass
+{
+    class MyBook : Book
+    {
+        protected int price;
+
+        public MyBook(String title, String author, int price) : base(title, author)                 //Same as super() in abstract extend in Java
+        {
+            this.price = price;
+        }
+
+        //Override abstract method in Book.cs
+        public override void display()
+        {
+            Console.WriteLine("Title: {0}\nAuthor: {1}\nPrice: {2}", title, author, price);
+        }
+    }
+}
---
+++ Day13AbstractClass/Day13AbstractClass/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day13AbstractClass
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
