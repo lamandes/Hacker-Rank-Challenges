132e78dd30af98b14539bda18c7cafe2c1534862
Andes Lam <lamandes@outlook.com>
2019-01-18 16:30:20 +0000

    Completed and commit Day 14 code.

--- Day14Scope/Day14Scope/Difference.cs
+++ Day14Scope/Day14Scope/Difference.cs
@@ -8,5 +8,29 @@ namespace Day14Scope
 {
     class Difference
     {
-    }
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] elements)
+        {
+            this.elements = elements;
+        }
+
+        public void computeDifference()
+        {
+            //O(n(n-1) runtime as it is unsorted and requires nested for loop each element
+            for (int i = 0; i < elements.Length; i++)
+            {
+                for (int j = i + 1; j < elements.Length; j++)
+                {
+                    int dif = Math.Abs(elements[i] - elements[j]);
+                    if (dif > maximumDifference)
+                    {
+                        maximumDifference = dif;
+                    }
+                }
+            }
+        }
+        
+    } 
 }
--- Day14Scope/Day14Scope/Solution.cs
+++ Day14Scope/Day14Scope/Solution.cs
@@ -10,6 +10,15 @@ namespace Day14Scope
     {
         static void Main(string[] args)
         {
+            Convert.ToInt32(Console.ReadLine());
+
+            int[] a = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToArray();
+
+            Difference d = new Difference(a);
+
+            d.computeDifference();
+
+            Console.Write(d.maximumDifference);
         }
     }
 }
