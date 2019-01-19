51897b3f64ca480db370f8dd54be1e930cf5f763
Andes Lam <lamandes@outlook.com>
2019-01-19 14:29:58 +0000

    Added skeleton of code.

---
+++ Day22BinarySearchTree/Day22BinarySearchTree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22BinarySearchTree
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
--- Day22BinarySearchTree/Day22BinarySearchTree/Solution.cs
+++ Day22BinarySearchTree/Day22BinarySearchTree/Solution.cs
@@ -8,8 +8,45 @@ namespace Day22BinarySearchTree
 {
     class Solution
     {
-        static void Main(string[] args)
+        static int getHeight(Node root)
         {
+            //Write your code here
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+
         }
     }
 }
