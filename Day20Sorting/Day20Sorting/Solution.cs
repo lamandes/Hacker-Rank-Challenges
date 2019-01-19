f3a52fa3ad423ce24c373fbba98ca66965c704db
Andes Lam <lamandes@outlook.com>
2019-01-19 12:39:58 +0000

    Created day 20 sorting project.

---
+++ Day20Sorting/Day20Sorting/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] a_temp = Console.ReadLine().Split(' ');
+        int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+        // Write Your Code Here
+    }
+}
