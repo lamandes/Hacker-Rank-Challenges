743f7ce27f8b68ad1ea1bafd54c12e8c1d3e663a
Andes Lam <lamandes@outlook.com>
2019-01-19 12:16:51 +0000

    Created day 19 interface code. Initial Commit.

---
+++ Day19Interfaces/Day19Interfaces/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19Interfaces
+{
+    class Calculator : AdvancedArithmetic
+    {
+        public int divisorSum(int n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Day19Interfaces/Day19Interfaces/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day19Interfaces
+{
+    class Solution
+    {
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+    }
+}
