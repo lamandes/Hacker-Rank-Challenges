56404039450de661c4dab90b67ff8314c22fb258
Andes Lam <lamandes@outlook.com>
2019-01-21 16:26:07 +0000

    Completed day 26 nested logitc. Commit code.

--- Day26NestedLogic/Day26NestedLogic/Solution.cs
+++ Day26NestedLogic/Day26NestedLogic/Solution.cs
@@ -8,8 +8,46 @@ namespace Day26NestedLogic
 {
     class Solution
     {
+        static int caculateFine(string[] szActual, string[] szExpected)
+        {
+            //szActual, szExpected [0]  = Day
+            //szActual, szExpected [1]  = Month
+            //szActual, szExpected [2]  = Year
+            int fine = 0;
+            int actualDay, actualMonth, actualYear;
+            int expectedDay, expectedMonth, expectedYear;
+            actualDay = int.Parse(szActual[0]);
+            actualMonth = int.Parse(szActual[1]);
+            actualYear = int.Parse(szActual[2]);
+            expectedDay = int.Parse(szExpected[0]);
+            expectedMonth = int.Parse(szExpected[1]);
+            expectedYear = int.Parse(szExpected[2]);
+
+            if (actualYear > expectedYear)
+            {
+                fine = 10000;
+            }
+            else if ((actualYear == expectedYear) && (actualMonth > expectedMonth))
+            {
+                fine = 500 * (actualMonth - expectedMonth);
+            }
+            else if ((actualYear == expectedYear) && (actualMonth == expectedMonth) && (actualDay > expectedDay))
+            {
+                fine = 15 * (actualDay - expectedDay);
+            }
+            else
+            {
+                fine = 0;
+            }
+            return fine;
+        }
+
         static void Main(string[] args)
         {
+            string[] szActualReturn = Console.ReadLine().Split(' ').ToArray();
+            string[] szExpectedReturn = Console.ReadLine().Split(' ').ToArray();
+
+            Console.WriteLine(caculateFine(szActualReturn, szExpectedReturn));
         }
     }
 }
