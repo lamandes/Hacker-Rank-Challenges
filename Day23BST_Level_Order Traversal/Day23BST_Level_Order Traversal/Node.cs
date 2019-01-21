d2ea766f471ea6c56a86e0d3ec479e2686c474bb
Andes Lam <lamandes@outlook.com>
2019-01-21 11:37:53 +0000

    Completed day 23 BST Level Order Traversal. Commit code.

---
+++ Day23BST_Level_Order Traversal/Day23BST_Level_Order Traversal/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23BST_Level_Order_Traversal
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+}
--- Day23BST_Level_Order Traversal/Day23BST_Level_Order Traversal/Solution.cs
+++ Day23BST_Level_Order Traversal/Day23BST_Level_Order Traversal/Solution.cs
@@ -8,8 +8,72 @@ namespace Day23BST_Level_Order_Traversal
 {
     class Solution
     {
-        static void Main(string[] args)
+        static void levelOrder(Node root)
         {
+            string szString = "";
+            Queue<Node> queue = new Queue<Node>();
+            if (root != null)
+            {
+                queue.Enqueue(root);
+                //Continue when queue is not empty
+                while (queue.Count != 0)
+                {
+                    //1. Get current root
+                    Node currentRoot = queue.Dequeue();
+
+                    //2. Process current root
+                    szString += currentRoot.data + " ";
+
+                    //3. Add left to queue
+                    if (currentRoot.left != null)
+                    {
+                        queue.Enqueue(currentRoot.left);
+                    }
+                    //4. Add right to queue
+                    if (currentRoot.right != null)
+                    {
+                        queue.Enqueue(currentRoot.right);
+                    }
+                }
+            }
+
+            //5. Finally, print out string
+            Console.WriteLine(szString);
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            levelOrder(root);
+
         }
     }
 }
