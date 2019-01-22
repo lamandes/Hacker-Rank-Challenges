1794d1b8e9ddcb1c80ed2b32cce7ade80b0eb06a
Andes Lam <lamandes@outlook.com>
2019-01-22 16:29:16 +0000

    Completed day 29 bitwise and project. Commit code.

--- Day29BitwiseAnd/Day29BitwiseAnd/Solution.cs
+++ Day29BitwiseAnd/Day29BitwiseAnd/Solution.cs
@@ -10,6 +10,31 @@ namespace Day29BitwiseAnd
     {
         static void Main(string[] args)
         {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                string[] nk = Console.ReadLine().Split(' ');
+
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                int maxNum = -1;
+                //Between 1 to n
+                for (int i = 1; i < n + 1; i++)
+                {
+                    for (int j = i + 1; j < n + 1; j++)
+                    {
+                        int curNum = i & j;
+                        if (curNum > maxNum && curNum < k)
+                        {
+                            maxNum = curNum;
+                        }
+                    }
+                }
+                Console.WriteLine(maxNum);
+            }
         }
     }
 }
