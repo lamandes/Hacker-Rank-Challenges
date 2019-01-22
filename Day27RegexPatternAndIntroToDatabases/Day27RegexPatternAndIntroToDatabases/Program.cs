9403c37843c069a42b68fffc40afdbe53ebfcf82
Andes Lam <lamandes@outlook.com>
2019-01-22 15:55:15 +0000

    Created day 27 regex pattern unit test project. Completed day 27 challenge.

--- Day27RegexPatternAndIntroToDatabases/Day27RegexPatternAndIntroToDatabases/Program.cs
+++ Day27RegexPatternAndIntroToDatabases/Day27RegexPatternAndIntroToDatabases/Program.cs
@@ -1,15 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+using System;
 
 namespace Day27RegexPatternAndIntroToDatabases
 {
-    class Program
+    public class Solution
     {
-        static void Main(string[] args)
+        //private varaiables
+        private ArrayList array;
+
+        //public constructor
+        public Solution()
         {
+            this.array = new ArrayList();
+        }
+
+        public void insertGmail(string firstName, string emailID)
+        {
+            string pattern = @"\b(\w+?)\@gmail.com";
+
+            //Regular expression that matches Anything@gmail.com
+            Regex rx = new Regex(pattern);
+            Match match = rx.Match(emailID);
+            if (match.Length != 0)
+            {
+                array.Add(firstName);
+            }
+            //else do nothing, no insert to array
+        }
+
+        public ArrayList getArray()
+        {
+            return array;
+        }
+
+        public void sort()
+        {
+            array.Sort();
+        }
+
+        /**
+         * Main method
+         **/
+        public static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            //Create an arraylist for storing all @gmail.com and sorting later
+            Solution sol = new Solution();
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+                string firstName = firstNameEmailID[0];
+
+                string emailID = firstNameEmailID[1];
+                sol.insertGmail(firstName, emailID);
+            }
+
+            //Sort arraylist
+            sol.sort();
+            foreach (string s in sol.getArray())
+            {
+                Console.WriteLine(s);
+            }
         }
     }
 }
+
+
+
+
---
+++ Day27RegexPatternAndIntroToDatabases/Day27RegexPatternAndIntroToDatabasesTests/SolutionTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Day27RegexPatternAndIntroToDatabases;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day27RegexPatternAndIntroToDatabases.Tests
+{
+    [TestClass()]
+    public class SolutionTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            //Arrange data
+            string testString1 = "a abc@abc.com";
+            string testString2 = "z z@gmail.com";
+            string testString3 = "t test@gmail.com";
+            string[] test1 = testString1.Split(' ');
+            string[] test2 = testString2.Split(' ');
+            string[] test3 = testString3.Split(' ');
+            string[] expected = { "t", "z" };
+            Solution sol = new Solution();
+            //Act
+            sol.insertGmail(test1[0], test1[1]);
+            sol.insertGmail(test2[0], test2[1]);
+            sol.insertGmail(test3[0], test3[1]);
+            sol.sort();
+            //Assert
+            //Assert.Equals or Assert.AreEqual will fail as it compare the object reference memory
+            CollectionAssert.Equals(expected, sol.getArray().ToArray());
+        }
+    }
+}
